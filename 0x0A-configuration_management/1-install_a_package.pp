5f359f95b6e3362a89031f1c0d40c96157c8090c
mikek1337 <mikekegnlyew@gmail.com>
2023-01-11 22:32:29 +0300

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,8 +2,8 @@
 exec{ 'pip-install':
     command => '/usr/bin/pip3 install flask'
     }
-package{ 'flask':
-    ensure  => installed,
-    require => Exec['pip-install'],
+    #package{ 'flask':
+        #ensure  => installed,
+        #require => Exec['pip-install'],
 
-    }
+        #}
