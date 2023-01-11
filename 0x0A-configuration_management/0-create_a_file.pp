48e57a5995768b24b1064132c126a7483d65fe92
mikek1337 <mikekegnlyew@gmail.com>
2023-01-11 21:08:46 +0300

    fix code

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,8 +3,8 @@ file { '/tmp/school' :
     ensure  => file,
     path    => '/tmp/school',
     mode    => '0744',
-    owner   => www-data,
-    group   => www-data,
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I Love Puppet'
 }
 
