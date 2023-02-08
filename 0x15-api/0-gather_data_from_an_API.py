ae8645f3f05571baea8d4a07bbfe88fc6aca11ef
mikek1337 <mikekegnlyew@gmail.com>
2023-02-08 20:35:04 +0300

    tesd

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
                 y += 1
             return y
         completed = list(map(count_completed, todos_json)).count(1)
-        print("Employee {name} is done with tasks({completed}/{total}): "
+        print("Employee {name} is done with tasks({completed}/{total}):"
               .format(name=user_json['name'],
                       completed=completed, total=len(todos_json)))
         for todo in todos_json:
