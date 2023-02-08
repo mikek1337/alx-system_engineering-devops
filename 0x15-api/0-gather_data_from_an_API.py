a04e978c29bd4d909abd25d3f91a8649ddfc6aa7
mikek1337 <mikekegnlyew@gmail.com>
2023-02-08 20:06:31 +0300

    fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,27 +4,22 @@
 import requests
 import json
 import sys
-id = sys.argv[1]
+if __name__ == "__main__":
+    id = sys.argv[1]
+    res = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{id}".format(id=id))
+    user_json = res.json()
+    res = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={id}".format(id=id))
+    todos_json = res.json()
 
-res = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{id}".format(id=id))
-user_json = res.json()
-
-res = requests.get(
-    "https://jsonplaceholder.typicode.com/todos?userId={id}".format(id=id))
-todos_json = res.json()
-
-
-def count_completed(x, y=0):
-    if x['completed']:
-        y += 1
-    return y
-
-
-completed = list(map(count_completed, todos_json)).count(1)
-
-print(
-    "Employee {name} is done with tasks({completed}/{total}):".format(name=user_json['name'], completed=completed, total=len(todos_json)))
-for todo in todos_json:
-    if todo['completed']:
-        print("\t {title}".format(title=todo['title']))
+    def count_completed(x, y=0):
+        if x['completed']:
+            y += 1
+            return y
+    completed = list(map(count_completed, todos_json)).count(1)
+    print(
+        "Employee {name} is done with tasks({completed}/{total}):".format(name=user_json['name'], completed=completed, total=len(todos_json)))
+    for todo in todos_json:
+        if todo['completed']:
+            print("\t {title}".format(title=todo['title']))
