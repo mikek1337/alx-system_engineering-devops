52e69bce6c03362dae8131296afc0e72b41263ba
mikek1337 <mikekegnlyew@gmail.com>
2023-02-28 19:28:35 +0300

    changed to exe

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 
 """Get top ten hot subreddits"""
 
@@ -7,5 +7,6 @@ import requests
 
 def top_ten(subreddit):
     headers = {"user-agent": "alx-project", "content-type": "application/json"}
+    param = {"limit": "10"}
     res = requests.get("https://www.reddit.com/r/{}/hot/.json"
                        .format(subreddit), headers=headers)
