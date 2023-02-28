b04f91200f6d95acf60e64d2451338e202727661
mikek1337 <mikekegnlyew@gmail.com>
2023-02-28 19:58:44 +0300

    top ten subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,4 +9,8 @@ def top_ten(subreddit):
     headers = {"user-agent": "alx-project", "content-type": "application/json"}
     param = {"limit": "10"}
     res = requests.get("https://www.reddit.com/r/{}/hot/.json"
-                       .format(subreddit), headers=headers)
+                       .format(subreddit), headers=headers, params=param)
+    json_result = res.json()
+    data = json_result.get('data')
+    children = data.get('children')
+    print("\n".join([child.get('data').get('title') for child in children]))
