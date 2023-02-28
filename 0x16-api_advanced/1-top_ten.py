b332ac5d9130fb88257ac1d7a3212779499f38ce
mikek1337 <mikekegnlyew@gmail.com>
2023-02-28 20:09:29 +0300

    top ten subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,10 +6,13 @@ import requests
 
 
 def top_ten(subreddit):
+    """Get top ten"""
     headers = {"user-agent": "alx-project", "content-type": "application/json"}
     param = {"limit": "10"}
     res = requests.get("https://www.reddit.com/r/{}/hot/.json"
                        .format(subreddit), headers=headers, params=param)
+    if res.status_code == 404 or res.status_code == 301:
+        print(None)
     json_result = res.json()
     data = json_result.get('data')
     children = data.get('children')
