be6e53221e38c10e36f1f89b90b8120aa78236ef
mikek1337 <mikekegnlyew@gmail.com>
2023-05-09 22:25:43 +0300

    counts

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,23 +1,43 @@
 #!/usr/bin/python3
-import re
+
 import requests
 """Simple module."""
 
 
 def count_words(subreddit, word_list):
     """Counts words in the subreddit."""
-    print(make_request(subreddit))
+    subreddit_content = make_request(subreddit).get("data").get("children")
+    titles = collect_titles(subreddit_content, [])
     print(word_list)
+    return count_subreddit(titles, word_list, {})
 
 
-def count_subreddit(subreddit_content, word_list, count_tracker):
-    if (len(word_list) == 0):
+def count_subreddit(titles, word_list, count_tracker):
+    if (len(titles) == 0):
         return count_tracker
-    word = word_list.pop()
-    found = len(re.fullmatch(word, subreddit_content, re.IGNORECASE))
-    if (found > 0):
-        count_tracker[word] = int(count_tracker[word]) + found
-        return count_subreddit(subreddit_content, word_list, count_tracker)
+    title = titles.pop()
+    words = word_list
+    print(count_exist(words, title, 0, 0))
+    return count_subreddit(titles, word_list, count_tracker)
+
+
+def count_exist(word_list, title, count, index):
+    if (len(word_list) == index):
+        return count
+    title = title.lower()
+    word = word_list[index].lower()
+    count += title.count(word)
+    index += 1
+    return count_exist(word_list, title, count, index)
+
+
+def collect_titles(children, titles):
+    if (len(children) == 0):
+        return titles
+    child = children.pop()
+    title = child.get("data").get("title")
+    titles.append(title)
+    return collect_titles(children, titles)
 
 
 def make_request(subreddit):
